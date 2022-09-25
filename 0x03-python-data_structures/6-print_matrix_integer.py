138e6f1fb0ab50178f4f5e8819481fa3e086b62c
Chijioke Chika <8pluscreative@gmail.com>
2022-09-25 12:08:00 +0000

    Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-    for x in range(len(matrix)):
-        for y in range(len(matrix[x])):
-            if y == x[-1]:
-                print("{:d}".format(matrix[x][y]), end='')
+    for row in matrix:
+        for column in row:
+            if column == row[-1]:
+                print('{:d}'.format(column), end='')
             else:
-                print('{:d}'.format(y), end=' ')
+                print('{:d}'.format(column), end=' ')
         print()
